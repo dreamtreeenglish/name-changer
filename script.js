2b89242d391e31b9c51d1ee3ff193a66c8db2328
Neil <dreamtreeenglish@gmail.com>
2025-02-07 12:47:57 +0900

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function changeName(callback){
+
+const lastName = document.getElementById('last-name');
+
+const firstName = document.getElementById('first-name');
+
+document.getElementById('full-english-name').innerHTML = 
+`${firstName.value} ${lastName.value}`
+
+document.getElementById('self-introduction').innerHTML = 
+`<strong>Introduction:</strong> Hi, I'm ${firstName.value} ${lastName.value}. Nice to meet you.`
+}
+
+
+function reset() {
+ location.reload();
+}
+
+document.getElementById("reset-button").addEventListener("click", reset);
+
